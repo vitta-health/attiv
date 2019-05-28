6832391f038d103aa4d33432875ef39e934ac341
Elvisley <elvis.souza@vitta.me>
2019-05-28 14:26:03 -0300

    feat: adicionando parametro para iniciar mongodb

--- framework/infrastructure/database/mongodb/DbContextMongo.ts
+++ framework/infrastructure/database/mongodb/DbContextMongo.ts
@@ -1,21 +1,27 @@
 import * as mongoose from 'mongoose';
-import { APIError } from '../../..';
+import { APIError, Attivlogger } from '../../..';
+import messages from '../../../crosscutting/messages/message';
 
 export default class DbContextMongo {
-  constructor() {
-    this.connect();
+  constructor({ config }) {
+    Attivlogger.info(` ${messages.DbContextoMongo.CONNECTING}: ${config.MONGO_USING} `);
+    this.connect(config.MONGO_USING);
   }
 
-  connect() {
-    try {
-      const CONNECTION_STRING = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${
-        process.env.MONGO_HOST
-      }:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
+  connect(initialize) {
+    if (initialize === 'true') {
+      try {
+        const CONNECTION_STRING = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${
+          process.env.MONGO_HOST
+        }:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
 
-      mongoose.connect(CONNECTION_STRING);
-      return mongoose.connection;
-    } catch (error) {
-      throw new APIError(error);
+        mongoose.connect(CONNECTION_STRING);
+        return mongoose.connection;
+      } catch (error) {
+        throw new APIError(error);
+      }
+    } else {
+      throw new APIError(messages.DbContextoMongo.DISABLED_CONNECTION);
     }
   }
 }
