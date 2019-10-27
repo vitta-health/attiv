403e21d516f7c6a14a826f1b61b553f2bd17fb5d
Tacio Medeiros <tacio.medeiros@vitta.me>
2019-10-27 10:52:22 -0300

    fix: Uncomment files wrong commented last commit

--- example/src/index.ts
+++ example/src/index.ts
@@ -10,26 +10,26 @@ import ExerciseRepository from './repositoryMongo/exercise/ExerciseRepository';
 import ExerciseService from './domain/excercise/services/implementation/ExerciseService';
 import ExerciseController from './application/controllers/ExerciseController';
 
-//const db = require('./repository/database/models/index.js');
+const db = require('./repository/database/models/index.js');
 
 container.register({
   config: asValue(process.env),
-  /*  eventServiceProviderExample: asValue(new EventServiceProviderExample()),
-    taskController: asClass(TaskController).scoped(),
-    taskService: asClass(TaskService).scoped(),
-    taskRepository: asClass(TaskRepository).scoped(),
-  
-    exerciseRepository: asClass(ExerciseRepository).scoped(),
-    exerciseService: asClass(ExerciseService).scoped(),
-    exerciseController: asClass(ExerciseController).scoped(),*/
-  //db: asValue(db),
+  eventServiceProviderExample: asValue(new EventServiceProviderExample()),
+  taskController: asClass(TaskController).scoped(),
+  taskService: asClass(TaskService).scoped(),
+  taskRepository: asClass(TaskRepository).scoped(),
+
+  exerciseRepository: asClass(ExerciseRepository).scoped(),
+  exerciseService: asClass(ExerciseService).scoped(),
+  exerciseController: asClass(ExerciseController).scoped(),
+  db: asValue(db),
 });
 
 const vittaRouter = container.resolve('router');
 
 vittaRouter.use(bodyParser.json());
-//vittaRouter.use('/task', container.resolve('taskController').getRouter());
-//vittaRouter.use('/exercise', container.resolve('exerciseController').getRouter());
+vittaRouter.use('/task', container.resolve('taskController').getRouter());
+vittaRouter.use('/exercise', container.resolve('exerciseController').getRouter());
 
 const app = container.resolve('app') as Application;
 
