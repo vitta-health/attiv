b066387e6f105587915ac7fbbeab06186e32bd76
Gilson Fernandes Batista de Souza <gilson@vitta.com.br>
2020-07-14 09:11:59 -0300

    fix: AjustesPayloadSize

--- framework/application/server.ts
+++ framework/application/server.ts
@@ -17,10 +17,10 @@ class Server {
     this.express.use(morgan('combined'));
     this.express.use(containerMiddleware);
     this.express.use(responseHandler);
-    this.express.use(router);
     this.express.use(cors);
-    this.express.use(bodyParser);
-    this.express.use(bodyParser.urlencoded({ extended: false }));
+    this.express.use(bodyParser.urlencoded({ limit: '100mb', extended: false }));
+    this.express.use(bodyParser.json({ limit: '100mb' }));
+    this.express.use(router);
     this.express.use('/docs', express.static('docs'));
 
     // catch 404 and forward to error handler
@@ -32,11 +32,11 @@ class Server {
   }
 
   getExpress() {
-    return express;
+    return this.express;
   }
 
   start() {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       const http = this.express.listen(process.env.API_PORT || 3000, () => {
         const { port } = http.address();
         console.info(`processo [${process.pid}] executando na porta ${port}`);
